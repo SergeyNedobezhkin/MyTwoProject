54935e8022855a7ad450e4d44e7cf80b29b2caec
Sergey Nedobezhkin <zelenka1633@gmail.com>
2022-10-27 21:26:09 +0400

    Выполнил задания lesson20

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -3,38 +3,24 @@ const modal = () => {
     const buttons = document.querySelectorAll('.popup-btn');
     const closeBtn = modal.querySelector('.popup-close');
     const popupContent = document.querySelector('.popup-content');
-    console.log(screen);
+
     const modalMenu = () => {
-        if(screen.width > 768){
-            let start = Date.now(); // запомнить время начала
-            let timer = setInterval(function () {
-                // сколько времени прошло с начала анимации?
-                let timePassed = Date.now() - start;
-    
-                if (timePassed >= 500) {
-                    clearInterval(timer); // закончить анимацию через 2 секунды
-                    return;
-                }
-    
-                // отрисовать анимацию на момент timePassed, прошедший с начала анимации
-                draw(timePassed);
-            }, 1);
-            // в то время как timePassed идёт от 0 до 2000
-            // left изменяет значение от 0px до 400px
-        }
-             
-        function draw(timePassed) {
-            popupContent.style.left = timePassed / 0.8 + 'px';
-        }
-
+        let count = 0;
+        const animate = () => {
+            count++;
+            if (count < 62 && screen.width > 768) {
+                popupContent.style.left = count * 0.624 + '%';
+            } else {
+                popupContent.style.left = " ";
+            }
+        };
+        setInterval(animate, 2);
     };
-
 
     buttons.forEach(btn => {
 
         btn.addEventListener('click', () => {
             modal.style.display = 'block';
-
             modalMenu();
         });
 
@@ -43,6 +29,5 @@ const modal = () => {
         });
 
     });
-
 };
 export default modal;
