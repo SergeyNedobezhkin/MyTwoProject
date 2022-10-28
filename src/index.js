fa49cdb8149a0b5cae70b0979c0f73063217752e
Sergey Nedobezhkin <zelenka1633@gmail.com>
2022-10-28 19:09:08 +0400

    Выполниз задания lesson21

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 import timer from './modules/timer';
 import menu from './modules/menu';
 import modal from './modules/modal';
+import calculator from './modules/calculator';
 timer('29 october 2022');
 menu();
 modal();
+calculator();
