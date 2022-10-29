33c2301ed142532dd5bcdc425da1c7c8afd4720a
Sergey Nedobezhkin <zelenka1633@gmail.com>
2022-10-29 11:18:07 +0400

    Устранил замечания lesson19

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import timer from './modules/timer';
 import menu from './modules/menu';
 import modal from './modules/modal';
 import calculator from './modules/calculator';
-timer('29 october 2022');
+timer('30 october 2022');
 menu();
 modal();
 calculator();
--- src/modules/timer.js
+++ src/modules/timer.js
@@ -1,44 +1,49 @@
 
 let idInterval = 0;
 
-const timer = (deadline) => {
-
+const timer = deadline => {
     const timerHours = document.getElementById('timer-hours');
     const timerMinutes = document.getElementById('timer-minutes');
     const timerSeconds = document.getElementById('timer-seconds');
 
-    const getTimeRemaining = () => {
-        let dateStop = new Date(deadline).getTime();
-        let dateNow = new Date().getTime();
-        let timeRemaining = (dateStop - dateNow) / 1000;
-        let hours = Math.floor(timeRemaining / 60 / 60);
-        let minutes = Math.floor((timeRemaining / 60) % 60);
-        let seconds = Math.floor(timeRemaining % 60);
-        return { timeRemaining, hours, minutes, seconds };
-    };
+    let idInterval = 0;
+    function getTimeRemaining() {
+        const dateStop = new Date(deadline).getTime();
+        const dateNow = new Date().getTime();
+        const timeRemaining = (dateStop - dateNow) / 1000;
+        const seconds = Math.floor(timeRemaining % 60);
+        const minutes = Math.floor((timeRemaining / 60) % 60);
+        const hours = Math.floor(timeRemaining / 60 / 60);
 
-    function addZero(num) {
-        if (num > 0 && num < 10) {
-            return '0' + num;
+        return { timeRemaining, hours, minutes, seconds };
+    }
+    function addZero(number) {
+        if (String(number).length === 1) {
+            return '0' + number;
         } else {
-            return num;
+            return String(number);
         }
     }
 
     const updateClock = () => {
-        let getTime = getTimeRemaining();
+        const getTime = getTimeRemaining();
 
-        timerHours.textContent = addZero(getTime.hours);
-        timerMinutes.textContent = addZero(getTime.minutes);
-        timerSeconds.textContent = addZero(getTime.seconds);
-
-        if (getTime.timeRemaining < 0) {
+        if (getTime.timeRemaining > 0) {
+            timerHours.textContent = addZero(getTime.hours);
+            timerMinutes.textContent = addZero(getTime.minutes);
+            timerSeconds.textContent = addZero(getTime.seconds);
+        } else if (getTime.timeRemaining <= 0) {
             clearInterval(idInterval);
+            // const dateStop = new Date(deadline);
+            // dateStop.setDate(dateStop.getDate() + 1);
+            timerHours.textContent = '00';
+            timerMinutes.textContent = '00';
+            timerSeconds.textContent = '00';
         }
-       
     };
-    idInterval = setInterval(updateClock, 1000, timer);
+    idInterval = setInterval(updateClock, 1000);
 };
+
 export default timer;
 
 
