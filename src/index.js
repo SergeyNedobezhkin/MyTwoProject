0b3b20c98338cc52edee06bc2f48382aae9b51ea
Sergey Nedobezhkin <zelenka1633@gmail.com>
2022-10-29 11:32:56 +0400

    Устранил замечания lesson19

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@
 import timer from './modules/timer';
 import menu from './modules/menu';
 import modal from './modules/modal';
-<<<<<<< HEAD
-=======
 import calculator from './modules/calculator';
->>>>>>> lesson21
 timer('30 october 2022');
 menu();
 modal();
 calculator();
+
