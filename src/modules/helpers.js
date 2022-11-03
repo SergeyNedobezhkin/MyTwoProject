14b5f2b08cb744f4918dd5f97cc301461456089d
Sergey Nedobezhkin <zelenka1633@gmail.com>
2022-11-03 12:54:05 +0400

    выполнил задание lesson25

--- src/modules/helpers.js
+++ src/modules/helpers.js
@@ -1,22 +1,19 @@
 
-const animate = () => {
-    const popupContent = document.querySelector('.popup-content');
-    
-    let count = 0;
-    let idInterval;
-    const animate = () => {
-        count++;
-        idInterval = requestAnimationFrame(animate);
+const popupContent = document.querySelector('.popup-content');
+let count = 0;
+let idInterval;
 
-        if (count < 41 && screen.width > 768) {
-            popupContent.style.left = count * 1 + '%';
-        } else if (count > 41 && screen.width < 768) {
-            popupContent.style.left = " ";
-        } else {
-            cancelAnimationFrame(idInterval);
-        }
-    };
-    animate();
+const animate = () => { 
+    count++;
+    idInterval = requestAnimationFrame(animate);
+
+    if (count < 41 && screen.width > 768) {
+        popupContent.style.left = count * 1 + '%';
+    } else if (count > 41 && screen.width < 768) {
+        popupContent.style.left = " ";
+    } else {
+        cancelAnimationFrame(idInterval);
+    }
 };
 
 export { animate };
